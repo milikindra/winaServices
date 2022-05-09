803ede661abcd4544c40e869c9f6aa5ebeb4a71f
vpa_indra <indra@viktori-automation.com>
2022-05-09 13:09:18 +0700

    inventory dan filter inventory

--- routes/web.php
+++ routes/web.php
@@ -22,10 +22,23 @@ $router->post('login', 'Login\LoginController@login');
 // $router->post('newPassword', 'Login\LoginController@newPassword');
 // $router->post('newPasswordSave', 'Login\LoginController@newPasswordSave');
 $router->group(['namespace' => 'Master'], function () use ($router) {
+    //employee
     $router->post('employee/getMenu', ['uses' => 'EmployeeController@getMenuList', 'as' => 'employee/getMenu']);
     $router->post('employee/getList', ['uses' =>  'EmployeeController@getList', 'as' => 'employee/getList']);
     $router->post('employeeAddSave', ['uses' => 'EmployeeController@employeeAddSave', 'as' => 'employeeAddSave']);
     $router->post('getEmployeeById', ['uses' => 'EmployeeController@getEmployeeById', 'as' => 'getEmployeeById']);
+
+    // inventory
+    $router->post('inventory/getList', ['uses' =>  'InventoryController@getList', 'as' => 'inventory/getList']);
+
+    // province
+    $router->post('getProvinceById', ['uses' => 'ProvinceController@getProvinceById', 'as' => 'getProvinceById']);
+
+    // kategori
+    $router->post('kategoriGetRawData', ['uses' => 'KategoriController@kategoriGetRawData', 'as' => 'kategoriGetRawData']);
+
+    // subkategori
+    $router->post('subKategoriGetRawData', ['uses' => 'SubKategoriController@subKategoriGetRawData', 'as' => 'subKategoriGetRawData']);
 });
 // $router->group(['middleware' => ['auth']], function () use ($router) {
 //     $router->group(['namespace' => 'Master'], function () use ($router) {
