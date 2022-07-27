2985efdbe5420eee13a46672a4e555814056204a
vpa_indra <indra@viktori-automation.com>
2022-07-27 09:03:26 +0700

    fix ship to on edit so

--- app/Http/Controllers/Transaction/SalesOrderController.php
+++ app/Http/Controllers/Transaction/SalesOrderController.php
@@ -164,7 +164,7 @@ class SalesOrderController extends Controller
     {
         $head = salesOrder::leftJoin('mascustomer', 'kontrak_head.ID_CUST', 'mascustomer.ID_CUST')
             ->where('kontrak_head.NO_BUKTI', $request->NO_BUKTI)
-            ->select('kontrak_head.*', 'mascustomer.ALAMAT1', 'mascustomer.al_npwp')
+            ->select('kontrak_head.*', 'mascustomer.ALAMAT1', 'mascustomer.ALAMAT2', 'mascustomer.KOTA', 'mascustomer.PROPINSI', 'mascustomer.al_npwp')
             ->get();
         $detail = salesOrderDetail::leftJoin('stock', 'stock.no_stock', 'kontrak_det.NO_STOCK')
             ->where('kontrak_det.NO_BUKTI', $request->NO_BUKTI)
